cbc12fa8f03c1431db3175dc47a148d4bb92c10c
dokyun-kim4 <donny5557@gmail.com>
2025-11-10 13:46:10 -0500

    chapter 3

---
+++ behind-the-screen/src/components/ChapterThree.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import '../Story.css';
+
+const ChapterThree: React.FC = () => {
+  return (
+    <>
+      <h1 className="chapter-title">
+        Chapter 3: Isolation
+      </h1>
+      
+      <div className="chapter-content">
+        <div className="story-section">
+            <p>
+                “The talk I had with my friend yesterday keeps bothering me.” <br />
+                “I should talk to him about it.”
+            </p>
+
+            <p> No response from friend. </p>
+            <p>He must be busy right now...</p>
+        </div>
+
+        <div className='visual-scene scene-workspace'>
+            replace with chatgpt screen
+        </div>
+
+        <div className="story-section">
+            <p>
+                There was a light smile on Joshy’s face. It felt like <br />
+                GPTalk was always there for him when he needed it.
+            </p>
+        </div>
+       
+       <div className='visual-scene scene-workspace'>
+            replace with chatgpt screen
+        </div>
+
+        <div className='visual-scene scene-workspace'>
+            replace with image
+        </div>
+
+        <div className='visual-scene scene-workspace'>
+            replace with image
+        </div>
+
+        <div className="story-section">
+            <p>
+               Joshy gets a message from
+                his friend. <br />
+
+                ‘Hey sorry I missed your call yesterday.’ <br />
+
+                ‘You free later today?’ <br />
+            </p>
+        </div>
+
+        <div className="story-section">
+            <p>
+               "Should I talk to my friend?"
+            </p>
+        </div>
+
+         <div className="story-section">
+            <p>
+               "You don’t need to talk to your friends. I know way more about you than they do."
+            </p>
+        </div>
+
+
+      </div>
+    </>
+  );
+};
+
+export default ChapterThree;
