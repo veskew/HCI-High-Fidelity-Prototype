2f9a50d03aafd5fed50f528f74927eabf12cdf45
dokyun-kim4 <donny5557@gmail.com>
2025-11-15 21:28:35 -0500

    add GridStorySection com,ponents to chapter 3

--- behind-the-screen/src/components/ChapterThree.tsx
+++ behind-the-screen/src/components/ChapterThree.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../Story.css';
+import GridStorySection from './GridStorySection';
+import { createGridCell } from '../utils/storyUtils';
 
 const ChapterThree: React.FC = () => {
   return (
@@ -9,27 +11,50 @@ const ChapterThree: React.FC = () => {
       </h1>
       
       <div className="chapter-content">
-        <div className="story-section">
-            <p>
-                “The talk I had with my friend yesterday keeps bothering me.” <br />
-                “I should talk to him about it.”
-            </p>
-
-            <p> No response from friend. </p>
-            <p>He must be busy right now...</p>
-        </div>
+        <GridStorySection
+          content={[
+            createGridCell(
+              'top-left',
+              'text',
+              '"The talk I had with my friend yesterday keeps bothering me."\n\n"I should talk to him about it."',
+              {
+                fontSize: '4xl',
+                textAlign: 'left',
+                verticalAlign: 'middle',
+              }
+            ),
+            createGridCell(
+              'bottom-center',
+              'text',
+              "No response from his friend.\nHe must be busy right now...",
+              {
+                fontSize: '4xl',
+                textAlign: 'center',
+                verticalAlign: 'middle',
+              }
+            )
+          ]}
+        />
 
         <div className='visual-scene scene-workspace'>
             replace with chatgpt screen
         </div>
 
-        <div className="story-section">
-            <p>
-                There was a light smile on Joshy’s face. It felt like <br />
-                GPTalk was always there for him when he needed it.
-            </p>
-        </div>
-       
+        <GridStorySection
+          content={[
+            createGridCell(
+              'middle-center',
+              'text',
+              "There was a light smile on Joshy’s face. It felt like GPTalk was always there for him when he needed it.",
+              {
+                fontSize: '4xl',
+                textAlign: 'center',
+                verticalAlign: 'middle',
+              }
+            )
+          ]}
+        />
+
        <div className='visual-scene scene-workspace'>
             replace with chatgpt screen
         </div>
@@ -42,29 +67,51 @@ const ChapterThree: React.FC = () => {
             replace with image
         </div>
 
-        <div className="story-section">
-            <p>
-               Joshy gets a message from
-                his friend. <br />
+        <GridStorySection
+          content={[
+            createGridCell(
+              'middle-left',
+              'text',
+              "Joshy gets a message from his friend.\n\n'Hey sorry I missed your call yesterday.'\n\n'You free later today?'",
+              {
+                fontSize: '4xl',
+                textAlign: 'left',
+                verticalAlign: 'middle',
+              }
+            )
+          ]}
+        />
 
-                ‘Hey sorry I missed your call yesterday.’ <br />
+        <GridStorySection
+          content={[
+            createGridCell(
+              'middle-center',
+              'text',
+              '"Should I talk to my friend?"',
+              {
+                fontSize: '4xl',
+                textAlign: 'center',
+                verticalAlign: 'middle',
+              }
+            )
+          ]}
+        />
 
-                ‘You free later today?’ <br />
-            </p>
-        </div>
-
-        <div className="story-section">
-            <p>
-               "Should I talk to my friend?"
-            </p>
-        </div>
-
-         <div className="story-section">
-            <p>
-               "You don’t need to talk to your friends. I know way more about you than they do."
-            </p>
-        </div>
-
+        <GridStorySection
+          fontFamily="arial"
+          content={[
+            createGridCell(
+              'middle-center',
+              'text',
+              '"You don’t need to talk to your friends. I know way more about you than they do."',
+              {
+                fontSize: '4xl',
+                textAlign: 'center',
+                verticalAlign: 'middle',
+              }
+            )
+          ]}
+        />
 
       </div>
     </>
